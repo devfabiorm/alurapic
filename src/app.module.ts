2e7fb85ef887c1f8ba6eec54e562aadea9e53da0
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-10-13 12:08:17 -0300

    Created custom http exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception-filter.filter';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [],
   providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
